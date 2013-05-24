722ea288d4a7af5ab5023c0d08728c89a010b5a3
Vanderson Martins do Rosario <vandersonmr2@gmail.com>
2013-05-23 23:09:09 -0300

    Pogado mais passou TSP-NN

--- graphard/src/ruby/fileIO.rb
+++ graphard/src/ruby/fileIO.rb
@@ -1,7 +1,8 @@
 class PlanoCartesiano
-
+	attr_accessor :plano
         def initialize()
                 @plano = Hash.new
+		@dist = Hash.new
         end
 
         def addVertice(index,x,y)
@@ -11,18 +12,44 @@ class PlanoCartesiano
         def getVertice(index)
                 return @plano[index]
         end
+
 	def getAllVertice
-		@plano.each {|key, value| print "#{key} "; getValue(value)}
+		@plano.each {|key, value| print "#{key} | "; getValue(value)}
 	end
+
 	def getValue(value)
 		value.each do |v|
 			print "#{v} "
 		end
 		puts
 	end
+
+	def calcDistancias	
+		@plano.each do |key1, v1|
+			@plano.each do |key2 ,v2|
+				@dist[[key1.to_i,key2.to_i]] = ((Math.sqrt(
+						(v2[0].to_f - v1[0].to_f)**2 + 
+						(v2[1].to_f - v1[1].to_f)**2)
+					)+0.5).to_int	
+			end
+		end		
+	end
+	
+	def printDist	
+		@plano.each do |key1, v1|
+                       	@plano.each do |key2 ,v2|	
+				print "#{@dist[[key1.to_i,key2.to_i]]}"
+				puts
+			end
+		end
+	end
+	
+	def getDist
+		return @dist
+	end
 end
 
-class FileIO
+class FileTSP
         def lerCartesianoNoArquivo(inputFile)
                 plano = PlanoCartesiano.new
                 file = File.new(inputFile,"r")
@@ -35,6 +62,3 @@ class FileIO
         end
 end
 
-fileIO = FileIO.new
-plano = fileIO.lerCartesianoNoArquivo("arquivo")
-plano.getAllVertice
--- graphard/src/ruby/main.rb
+++ graphard/src/ruby/main.rb
@@ -1,12 +1,29 @@
+load 'fileIO.rb'
+load 'tspnn.rb'
 load 'fileEC.rb'
 load 'graph.rb'
 load 'hierholzer.rb'
 
-fileEC = FileEC.new
-graph = fileEC.lerGrafoNoArquivo(ARGV[1])
+if(ARGV[0]=="tsp-nn")
+	fileIO = FileTSP.new
+	graph = fileIO.lerCartesianoNoArquivo(ARGV[1])
+	graph.calcDistancias
 
-eulerian = hierholzer(graph.getGraph)
+	res = tspNN(graph)
+	custo = res[0]
+	tsp = res[1]
 
-eulerian.each do |v|
-	puts "#{v}"
+	puts "#{custo}"
+	tsp.each do |v|
+		puts "#{v}"
+	end
+elsif(ARGV[0]=="ec")
+	fileEC = FileEC.new
+	graph = fileEC.lerGrafoNoArquivo(ARGV[1])
+
+	eulerian = hierholzer(graph.getGraph)
+
+	eulerian.each do |v|
+		puts "#{v}"
+	end
 end
